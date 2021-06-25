66c9c98eeadacbb81733192ab7016c8c34d81341
nornia7 <lriozevtte@gmail.com>
2021-06-26 01:03:13 +0900

    コントローラー修正
    commentsコントローラーの不要記述削除
    prototypesコントローラー showアクションの@commentsのデータ取得が誤っていたため修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,8 +5,6 @@ class CommentsController < ApplicationController
     if @comment.save
       redirect_to prototype_path(@comment.prototype)
     else
-      @tweet = @comment.prototype
-      @comments = @prototype.comments
       render "prototypes/show"
     end
   end
